f4f1710bb3dfbd9b86f17d90b22cb1198f7c2d6b
Uzair Tariq <uzair0213@icloud.com>
2022-10-17 12:36:15 -0600

    Successfully got each safe waypoint correlated with the original name. Valid connections now outputs the connections by name rather than coordinates.

--- tree.py
+++ tree.py
@@ -27,49 +27,79 @@ def parse_data(filename: str):
     return nodes
 
 
-def find_valid_connections(nodes: dict, margin: float = 1.3):
+def create_safe_waypoints(nodes: dict, margin: float = 1.3):
     """
-    Finds all connections between any two nodes in the given data that don't intersect the polygon bounded by the
-    vertices labelled 'obstacle'.
-    TODO: find a way to correlate the new safe waypoints with the original 'waypoint_name'
+    Apply a safety margin to nodes so connections can be made without being on the border of the restricted region,
+    while preserving the name associated with each waypoint.
 
+    :param nodes: A dictionary of nodes that has the form {'waypoint_name': (x, y, classification)}
     :param margin: Amount to scale the restricted region by to provide a safety margin so the drone doesn't
         accidentally enter the restricted region.
-    :param nodes: Dictionary of parsed nodes from parse_data(): {'waypoint_name': (x, y, classification)}
-    :return: A list containing every two points that can be connected without intersecting the restricted region. Each
-        pair of points is a list of two tuples representing (x, y).
+    :return: A dictionary containing each waypoint name and its corresponding safe waypoint. Also return the
+        restricted_region polygon for later use.
     """
 
     # list of (x,y) of obstacle bounds
     restricted_vertices = [(node[0], node[1]) for node in nodes.values() if node[2] == 'obstacle']
     restricted_region = sp.Polygon(*restricted_vertices)
 
+    safe_waypoints = dict()
     # list of (x,y) of START and TARGET
-    waypoints = [sp.Point((node[0], node[1])) for node in nodes.values() if node[2] != 'obstacle']
+    for name, data in nodes.items():
+        if data[2] != 'obstacle':
+            safe_waypoints[name] = (data[0], data[1])
 
     safe_region = restricted_region.scale(margin, margin, restricted_region.centroid)
+
     # ensure the safe_region is not so large that START or TARGET are inside
-    # TODO: This is not working yet
-    if safe_region.encloses_point(waypoints[0]) or safe_region.encloses_point(waypoints[1]):
-        return "!! Margin factor too large."
+    # TODO: Ensure safe_region does not enclose START and TARGET
 
     # extend waypoints to also include safe vertices
-    waypoints.extend(safe_region.vertices)
+    # IMPORTANT: this assumes that the order of points is maintained throughout this whole process
+    i = 0
+    for name, data in nodes.items():
+        if name not in safe_waypoints:
+            safe_waypoints[name] = (safe_region.vertices[i].x, safe_region.vertices[i].y)
+            i += 1
+        else:
+            pass
+
+    return safe_waypoints, restricted_region
+
+
+def find_valid_connections(safe_waypoints: dict, restricted_region: sp.Polygon):
+    """
+    Finds all connections between any two nodes in the given data that don't intersect the polygon bounded by the
+    vertices labelled 'obstacle'.
+
+    :param restricted_region: The region bounded by the 'obstacle' waypoints that must be avoided
+    :param safe_waypoints: A dictionary of waypoints and their names that has the safety margin applied
+    :return: A list containing every two points that can be connected without intersecting the restricted region. Each
+        pair of points is a tuple containing the names of the first and second waypoint that can be connected.
+    """
 
     # gives a list of all possible combinations of points
-    combos = list(combinations(waypoints, 2))
+    combos = list(combinations(safe_waypoints.values(), 2))
 
     valid_connections = []
+    waypoint_names = list(safe_waypoints.keys())
+    waypoint_values = list(safe_waypoints.values())
 
     for combo in combos:
         intersect = sp.intersection(sp.Segment(*combo), restricted_region)
         if not intersect:
-            valid_connections.append([i.args for i in combo])
+            # find first waypoint name
+            first = waypoint_values.index(combo[0])
+            # find second waypoint name
+            second = waypoint_values.index(combo[1])
+            # append ('first', 'second') to valid_connections
+            valid_connections.append((waypoint_names[first], waypoint_names[second]))
 
     return valid_connections
 
 
 if __name__ == "__main__":
     parsed_nodes = parse_data("data.csv")
-    print(find_valid_connections(parsed_nodes, margin=1.3))
-    # print(find_valid_connections(parsed_nodes, 100))
+    waypoints, nofly_region = create_safe_waypoints(parsed_nodes)
+    connections = find_valid_connections(waypoints, nofly_region)
+    print(len(connections), connections)
